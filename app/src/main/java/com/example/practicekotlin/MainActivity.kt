f160195fd56d5b177e87ee5216f969ad6a55faf7
Thogaruchsesti-Hemanth <saihemanth225@g,ail.com>
2025-06-28 19:51:29 +0530

    Compose Quadrant completed

--- app/src/main/java/com/example/practicekotlin/MainActivity.kt
+++ app/src/main/java/com/example/practicekotlin/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.practicekotlin
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.practicekotlin.ui.theme.PracticeKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +35,7 @@ class MainActivity : ComponentActivity() {
                    modifier = Modifier.fillMaxSize(),
                    color = MaterialTheme.colorScheme.background
                ) {
-                    BirthdayGreeting(
-                        name = "Android",
-                        from = "Hemanth",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                   ComposeQuadrant(modifier = Modifier.padding(16.dp))
                 }
             }
         }
@@ -44,31 +43,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreeting(name: String, from: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = "Hello $name!",
-            fontSize = 100.sp,
-            lineHeight = 166.sp,
-            textAlign =  TextAlign.Center
-        )
-        Text(
-            text = "from $from!",
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
+fun ComposeQuadrant(modifier: Modifier = Modifier) {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.fillMaxSize().weight(1f).background(Color(0xFFEADDFF))
+            ){
+                Text(
+                    text = "Text composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.",
+                    textAlign = TextAlign.Justify
+                )
 
-        )
+            }
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.fillMaxSize().weight(1f).background(Color(0xFFD0BCFF))
+            ) {
+                Text(
+                    text = "Image composable, " ,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.fillMaxSize().weight(1f).background(Color(0xFFB69DF8))
+            ) {
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier.fillMaxSize().weight(1f).background(Color(0xFFF6EDFF))
+            ) {
+
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
     }
-
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true
+)
 @Composable
-fun GreetingPreview() {
-    BirthdayGreeting(name = "SAI", from = "Hemanth")
+fun ComposeQuadrantPreview(){
+    ComposeQuadrant()
 }
