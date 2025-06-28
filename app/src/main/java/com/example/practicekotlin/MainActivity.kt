121d5762743d2f704af28f85ba5c1f5eb4da7272
Thogaruchsesti-Hemanth <saihemanth225@g,ail.com>
2025-06-28 15:07:44 +0530

    Task Completed

--- app/src/main/java/com/example/practicekotlin/MainActivity.kt
+++ app/src/main/java/com/example/practicekotlin/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.example.practicekotlin
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,11 +32,7 @@ class MainActivity : ComponentActivity() {
                    modifier = Modifier.fillMaxSize(),
                    color = MaterialTheme.colorScheme.background
                ) {
-                    BirthdayGreeting(
-                        name = "Android",
-                        from = "Hemanth",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                  TaskCompleted()
                 }
             }
         }
@@ -44,31 +40,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreeting(name: String, from: String, modifier: Modifier = Modifier) {
-    Column(
+fun TaskCompleted(modifier: Modifier =Modifier){
+    Column(modifier =  modifier,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = "Hello $name!",
-            fontSize = 100.sp,
-            lineHeight = 166.sp,
-            textAlign =  TextAlign.Center
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        val image = painterResource(R.drawable.ic_task_completed)
+        Image(
+            painter = image,
+            contentDescription = null,
         )
         Text(
-            text = "from $from!",
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.End)
-
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier =  modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
         )
     }
-
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    BirthdayGreeting(name = "SAI", from = "Hemanth")
+fun TaskCompletedPreview(){
+    TaskCompleted()
 }
